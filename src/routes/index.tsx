28f8a65bac449957e6943e701896047479b99297
Adam Rackis <adamrackis@hotmail.com>
2025-10-24 00:51:07 -0500

    start

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,21 @@ export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <div className="flex flex-col m-8">
-      <span>Hello</span>
-      <span>World</span>
+    <div className="m-8">
+      <div className="h-[500px] flex flex-col gap-2 overflow-auto">
+        <div className="flex flex-col gap-2 bg-blue-400 h-[400px] min-h-[400px]">
+          <span>Top</span>
+          <span className="mt-auto">Bottom</span>
+        </div>
+        <div className="flex flex-col gap-2 flex-1 bg-red-400 sticky top-0 h-[200px] min-h-[200px]">
+          <span>Top</span>
+          <span className="mt-auto">Bottom</span>
+        </div>
+        <div className="flex flex-col gap-2 bg-green-400 h-[400px] min-h-[400px]">
+          <span>Top</span>
+          <span className="mt-auto">Bottom</span>
+        </div>
+      </div>
     </div>
   );
 }
