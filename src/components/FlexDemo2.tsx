6959e4786ddfaaeb46c3c3f69a6c8248f8aa1964
Adam Rackis <adamrackis@hotmail.com>
2025-10-25 16:37:04 -0500

    work

--- src/components/FlexDemo2.tsx
+++ src/components/FlexDemo2.tsx
@@ -2,7 +2,7 @@ export const FlexDemo2 = () => {
   return (
     <div className="flex border-2 rounded-md">
       <div className="h-[600px] flex flex-col flex-1 gap-2 overflow-auto">
-        <div className="h-[300px] bg-gray-400 overflow-auto">
+        <div className="h-[300px] bg-gray-400">
           {Array.from({ length: 100 }).map((_, idx) => (
             <div className="flex gap-2">
               <span>TopContent line {idx}</span>
@@ -11,7 +11,7 @@ export const FlexDemo2 = () => {
         </div>
         <div className="h-[300px] flex sticky top-0">
           <div className="flex-3">Some content here</div>
-          <div className="flex-1 flex-col gap-2 overflow-auto">
+          <div className="flex-1 flex-col gap-2">
             {Array.from({ length: 100 }).map((_, idx) => (
               <div className="flex gap-2">
                 <span>Middle Content line {idx}</span>
@@ -19,7 +19,7 @@ export const FlexDemo2 = () => {
             ))}
           </div>
         </div>
-        <div className="h-[300px] bg-gray-400 overflow-auto">
+        <div className="h-[300px] bg-gray-400">
           {Array.from({ length: 100 }).map((_, idx) => (
             <div className="flex gap-2">
               <span>Bottom Content line {idx}</span>
